41f857a95cc31d403b2071fe626e5522c2e10f95
HoualefAkram <akram1232004@gmail.com>
2025-05-19 19:53:45 +0100

    removed banned letters

--- lib/cipher.dart
+++ lib/cipher.dart
@@ -1,30 +1,32 @@
 import 'dart:math';
-import "dart:developer" as dev;
+import 'dart:developer' as dev;
 
 class Cipher {
   static const int _asciiLen = 127;
+  static const int _maxShift = 10000;
+  // static final List<int> _removedAscii = List.generate(32, (index) => index)
+  //   ..add(127);
+  // static const int _spaceAscii = 32;
   static String encode(String text, int? seed) {
-    dev.log("encoding with seed: $seed");
     if (seed == null) return text;
     final Random rng = Random(seed);
     final List<int> ascii = text.codeUnits;
-    final List<int> shifted = ascii.map(
-      (e) {
-        final int shiftedAscii = _addToCode(e, rng.nextInt(_asciiLen));
-        return shiftedAscii;
-      },
-    ).toList();
+    final List<int> shifted = ascii
+        .map(
+          (e) => _addToCode(e, rng.nextInt(_maxShift)),
+        )
+        .toList();
+    dev.log(shifted.toString());
     return String.fromCharCodes(shifted);
   }
 
   static String decode(String hash, int? seed) {
-    dev.log("decoding with seed: $seed");
     if (seed == null) return hash;
     final Random rng = Random(seed);
     final List<int> ascii = hash.codeUnits;
     final List<int> shifted = ascii.map(
       (e) {
-        final int shiftedAscii = _substractFromCode(e, rng.nextInt(_asciiLen));
+        final int shiftedAscii = _substractFromCode(e, rng.nextInt(_maxShift));
         return shiftedAscii;
       },
     ).toList();
@@ -32,16 +34,15 @@ class Cipher {
   }
 
   static int _addToCode(int code, int value) {
-    if (code + value > _asciiLen) {
-      return code + value - _asciiLen - 1;
-    }
-    return code + value;
+    int output = (code + value) % _asciiLen;
+    // return _removedAscii.contains(output) ? _spaceAscii : output;
+    return output;
   }
 
   static int _substractFromCode(int code, int value) {
-    if (value > code) {
-      return _asciiLen + code - value + 1;
-    }
-    return code - value;
+    final int sub = code - value;
+    final output = (sub % _asciiLen + _asciiLen) % _asciiLen;
+    // return _removedAscii.contains(output) ? _spaceAscii : output;
+    return output;
   }
 }
